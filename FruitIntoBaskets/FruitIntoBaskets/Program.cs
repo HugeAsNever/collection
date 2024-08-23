8742a3eab69ad6140c4a10b6e789273ff4c156f2
Ping Guan <ping.guan@outlook.com>
2024-08-23 10:14:13 -0600

    20240823 fruit

---
+++ FruitIntoBaskets/FruitIntoBaskets/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FruitIntoBaskets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int TotalFruit(int[] fruits)
+            {
+                var fruitTypes = fruits.ToList().Distinct();
+                var maxNumber = 0;
+                var secondMaxNumber = 0;
+                foreach (var type in fruitTypes)
+                {
+                    var fruitCount = fruits.Select(x => x == type).ToList().Count;
+                    if (fruitCount > secondMaxNumber)
+                    {
+                        secondMaxNumber = fruitCount;
+                    }
+                    if (secondMaxNumber > maxNumber)
+                    {
+                        var temp = maxNumber;
+                        maxNumber = secondMaxNumber;
+                        secondMaxNumber = temp;
+                    }
+                }
+                return maxNumber + secondMaxNumber;
+            }
+        }
+    }
+}
